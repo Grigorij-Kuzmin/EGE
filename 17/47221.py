8356046c08508f09f32fee8dca9374ba73e1ac76
Grisha <grisha.kuzmin.gk@gmail.com>
2022-12-09 20:50:33 +0300

    09. 12.22

--- 17/47221.py
+++ 17/47221.py
@@ -1,9 +1,12 @@
-nums = list(map(int, open('17 (47221).txt').readlines()))
+nums = list(map(int, open('47221.txt').readlines()))
 count = 0
-m = max(nums)
-MaxSum = -10001
+m = -10001
+for i in range(len(nums)):
+    if int(abs(nums[i])) % 10 == 3:
+        m = max(m, int(nums[i]))
+maxSum = -10001
 for i in range(len(nums) - 1):
-    if (nums[i] ** 2 + nums[i + 1] ** 2 >= m ** 2) and ((nums[i] % 10 == 3 and nums[i + 1] % 10 != 3) or (nums[i] % 10 != 3 and nums[i + 1] % 10 == 3)):
+    if (nums[i] ** 2 + nums[i + 1] ** 2 >= m ** 2) and ((abs(nums[i]) % 10 == 3 and abs(nums[i + 1]) % 10 != 3) or (abs(nums[i]) % 10 != 3 and abs(nums[i + 1]) % 10 == 3)):
         count += 1
         maxSum = max(maxSum, nums[i] ** 2 + nums[i + 1] ** 2)
 print(count, maxSum)

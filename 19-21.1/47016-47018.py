9b58b959590f1294264984ce82b83c77a319bd7b
Grisha <grisha.kuzmin.gk@gmail.com>
2022-12-16 17:32:46 +0300

    16. 12.22

--- 19-21.1/47016-47018.py
+++ 19-21.1/47016-47018.py
@@ -1,12 +1,19 @@
-def game(heap, moves, step):
+def game(heap, moves, step, op):
     if heap >= 29:
         return moves % 2 == step % 2
     if moves == step:
         return 0
-    result = [game(heap + 1, moves + 1, step),
-              game(heap * 2, moves + 1, step),
-              game(heap + 2, moves + 1, step)]
-    return any(result) if ((moves + 1) % 2 == step % 2) else all(result)
+    result = []
+    if op == 1:
+        result = [game(heap + 2, moves + 1, step, op), game(heap * 2, moves + 1, step, op)]
+    elif op == 2:
+        result = [game(heap + 1, moves + 1, step, op), game(heap * 2, moves + 1, step, op)]
+    elif op == 3:
+        result = [game(heap + 1, moves + 1, step, op), game(heap + 2, moves + 1, step, op)]
+    if moves % 2 == 0:
+        return all(result)
+    else:
+        return any(result)
 
 
-print(f'19:{[for s in range(1, 29) if not game(s, 0, 1) and game(s, 0, 3)}')
+print(f'19: {min(s for s in range(1, 28) if not game(s, 0, 1, 0) and game(s, 0, 3, 0))}')

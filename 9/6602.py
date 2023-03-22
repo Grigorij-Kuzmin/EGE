d4d78a8e49bba7bb7e692f8f966b445599788b67
Grisha <grisha.kuzmin.gk@gmail.com>
2023-03-22 20:24:42 +0300

    22.03.23

--- 9/6602.py
+++ 9/6602.py
@@ -1,7 +1,7 @@
 count = 0
 with open('09.6602.txt') as f:
     for line in f:
-        nums = list(map(int, line.split('\t')))
+        nums = list(map(int, line.strip().split('\t')))
         if len(nums) - len(set(nums)) == 1:
             s = 2 * (sum(nums) - sum(set(nums)))
             if ((sum(set(nums)) - s / 2) / 4) >= s:
